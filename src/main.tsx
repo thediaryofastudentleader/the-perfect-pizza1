fe9cd192388da19836d51ce7bb23af0afeb837f6
thediaryofastudentleader <thediaryofastudentleader@gmail.com>
2026-06-24 10:33:00 +0200

    Change App import to named import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { App } from './App';  // Named import, not default
 
 const container = document.getElementById('app');
 if (container) {
