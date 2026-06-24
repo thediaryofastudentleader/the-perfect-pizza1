28b6e13179041648fa67ce7839788f3467fa5a79
thediaryofastudentleader <thediaryofastudentleader@gmail.com>
2026-06-24 11:20:02 +0200

    Import App.css for styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { PizzaProvider } from './context/PizzaContext';
 import { PizzaCanvas } from './components/PizzaCanvas';
 import { MenuSelector } from './components/MenuSelector';
